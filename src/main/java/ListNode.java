a1d80a967d370e8dbe7cbc52688842a4945f5c43
Oleksandr Zviriuk <sanyazveruk@mail.com>
2022-03-30 20:10:58 +0300

    test passing

---
+++ src/main/java/ListNode.java
@@ -0,0 +1,37 @@
+import org.jetbrains.annotations.NotNull;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+    public ListNode(int @NotNull [] nums) {
+        if (nums.length == 0) {
+            throw new IllegalArgumentException("nums is empty");
+        }
+        else {
+            this.val = nums[0];
+            ListNode head = this;
+            for (int i = 1; i < nums.length; i++) {
+                head.next = new ListNode(nums[i]);
+                head = head.next;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        ListNode head = this;
+        while (head != null) {
+            sb.append(head.val);
+            sb.append("->");
+            head = head.next;
+        }
+        sb.append("null");
+        return sb.toString();
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ListNode head = new ListNode(new int[]{1, 2, 3, 4, 5});
+//        var x = new ListNode(null);
+
+        var rv = new LinkedListRemover().removeNthFromEnd(head, 2);
+
+        System.out.println(rv);
     }
 }
